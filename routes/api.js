47e51ee80a0bd10eecf6921fdbffcac5a1e35039
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-07-20 19:28:09 -0400

    fixed indents

--- routes/api.js
+++ routes/api.js
@@ -8,37 +8,9 @@ var controllers = {
     startup: StartupController,
     profile: ProfileController
 }
-router.post('/:resource', function(req, res, next) {
-    var resource = req.params.resource
-    var controller = controllers[resource]
-    if (controller == null){
-        res.json({
-            confirmation: 'fail',
-            message: 'Invalid Request'
-        })
-        return
-      }
-
-      controller.post(req.body, function(err, result){
-          if (err){
-              res.json({
-                  confirmation: 'fail',
-                  message: err
-              })
-              return
-          }
-
-          res.json({
-              confirmation: 'success',
-              results: results
-          })
-          return
-      })
-  })
 
 router.get('/:resource', function(req, res, next) {
     var resource = req.params.resource
-
     var controller = controllers[resource]
     if (controller == null){
         res.json({
@@ -46,23 +18,53 @@ router.get('/:resource', function(req, res, next) {
             message: 'Invalid Request'
         })
         return
+    }
+
+    controller.get(req.query, function(err, results){
+        if (err){
+            res.json({
+                confirmation: 'fail',
+                message: err
+            })
+            return
+        }
+
+        res.json({
+            confirmation: 'success',
+            results: results
+        })
+
+        return
+    })
+})
+
+router.post('/:resource', function(req, res, next) {
+    var resource = req.params.resource
+    var controller = controllers[resource]
+      if (controller == null){
+          res.json({
+              confirmation: 'fail',
+              message: 'Invalid Request'
+        })
+        return
       }
 
-      controller.get(req.query, function(err, results){
-          if (err){
-              res.json({
-                  confirmation: 'fail',
-                  message: err
-              })
-              return
-          }
-
-          res.json({
-              confirmation: 'success',
-              results: results
+    controller.post(req.body, function(err, result){
+        if (err){
+            res.json({
+                confirmation: 'fail',
+                message: err
           })
           return
-      })
-  })
+        }
+
+        res.json({
+            confirmation: 'success',
+            results: results
+        })
+        return
+    })
+})
+
 
 module.exports = router
