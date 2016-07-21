c1dc7c07137564aa6c8641a608bdbb1089e2e0ee
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-07-20 20:45:10 -0400

    added images

--- models/Startup.js
+++ models/Startup.js
@@ -3,8 +3,10 @@ var mongoose = require('mongoose')
 var StartupSchema = new mongoose.Schema({
 	name: {type:String, default:''},
 	city: {type:String, default:''},
+	image: {type:String, default:'placeholder.jpg'},
 	founder: {type:String, default:''},
 	shares: {type:Number, default:0}
+
 })
 
 module.exports = mongoose.model('StartupSchema', StartupSchema)
